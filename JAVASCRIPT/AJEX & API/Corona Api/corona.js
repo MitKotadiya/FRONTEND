0f4bc60760fe4428137a20342f7ecb9e4ded307b
MitKotadiya <kotadiyamit17552@gmail.com>
2025-07-16 12:01:46 +0530

    Project Completed with aos animation

--- JAVASCRIPT/AJEX & API/Corona Api/corona.js
+++ JAVASCRIPT/AJEX & API/Corona Api/corona.js
@@ -13,7 +13,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
         .then(res => res.json()) 
         .then(data => { 
             let exit = true;
-            data.data.forEach((element) => {
+            data.data.forEach((element , index) => {
                 if (element.day === search) {
                     document.getElementById("total").innerHTML = element.summary.total;
                     document.getElementById("confirmed").innerHTML = element.summary.confirmedCasesIndian;
@@ -24,18 +24,28 @@ document.querySelector("form").addEventListener("submit", (event) => {
                     let statecard = document.querySelector("#statecard");
                     statecard.innerHTML = "";
 
-                    element.regional.forEach((region) => {
+                    element.regional.forEach((region , index) => {
                         let card = document.createElement("div");
                         card.classList.add("state-card");
 
+                            if (index % 3 === 0) {
+                                card.setAttribute("data-aos", "fade-right");
+                            } else if (index % 3 === 2) {
+                                card.setAttribute("data-aos", "fade-left");
+                            } else {
+                                card.setAttribute("data-aos", "zoom-in");
+                            }
+                            
                         card.innerHTML = `
                             <h4>${region.loc}</h4>
+                            <hr>
                             <p><strong>Confirmed:</strong> ${region.confirmedCasesIndian}</p>
                             <p><strong>Deaths:</strong> ${region.deaths}</p>
                             <p><strong>Discharged:</strong> ${region.discharged}</p>`;
                         
                         statecard.append(card);
                     });
+                    document.getElementById("search").value = "";
 
                     exit = false;
                     return;
@@ -43,7 +53,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
             });
 
             if (exit) {
-                alert("Data Not Found");
+                alert("Plase Enter Date between 10-March-2020 to 15-December-2021");
             }
         })
         .catch(error => {
