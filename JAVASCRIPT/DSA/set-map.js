3c7b449b33dd1fe08dcbf56e28920058d08665c4
MitKotadiya <kotadiyamit17552@gmail.com>
2025-08-21 11:53:46 +0530

    Set - Map Add

--- JAVASCRIPT/DSA/set-map.js
+++ JAVASCRIPT/DSA/set-map.js
@@ -18,7 +18,16 @@
 // map ---> map store unique key and value (object)
 
 
-let mapdata = new Map([["100 ", "Hundred"] , ["name" , "mit"]]);
+// let mapdata = new Map([["100 ", "Hundred"] , ["name" , "mit"]]);
 
-
-console.log(mapdata)
+// mapdata.set("200","Two")
+// mapdata.delete("200","Two")
+// console.log(mapdata.has("200","Two"));
+// console.log(mapdata.keys());
+// console.log(mapdata.values());
+// console.log(mapdata.size);
+// console.log(mapdata.get("100")); // return value as per key
+// mapdata.forEach((element,index)=> {
+//     console.log(`${index} --> ${element}`);
+// })
+// console.log(mapdata)
